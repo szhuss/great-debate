63444448924b84d4751a49350a749c36bc61de6c
Saudia Hussain Holmes <szhuss@hotmail.com>
2021-04-01 20:41:53 -0400

    img changes and styling updates

--- gd.js
+++ gd.js
@@ -16,7 +16,7 @@ function openPage(pageName,elmnt,color) {
     elmnt.style.backgroundColor = color;
   }
 
-  // Get the element with id="defaultOpen" and click on it
+// Get the element with id="defaultOpen" and click on it
 //   document.getElementById("defaultOpen").click();
 
 // ================================================================
